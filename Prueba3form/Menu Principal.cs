bc41e3e6af296f28b12aea15a121ece3f81a7f6d
mateogomezr <>
2019-11-13 19:14:53 -0300

    messagebox
    MessageBox.Show("Este boton aun no esta programado");

--- Prueba3form/Menu Principal.cs
+++ Prueba3form/Menu Principal.cs
@@ -20,112 +20,113 @@ namespace Prueba3form
 
         private void comprobanteToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            MessageBox.Show("Este boton aun no esta programado");
 
         }
 
         private void chequesToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Este boton aun no esta programado");
         }
 
         private void consultaToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Este boton aun no esta programado");
         }
 
         private void ordenDePagoToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Este boton aun no esta programado");
         }
 
         private void cobranzasToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Este boton aun no esta programado");
         }
 
         private void cargarToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Este boton aun no esta programado");
         }
 
         private void consultarToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Este boton aun no esta programado");
         }
 
         private void consultarToolStripMenuItem2_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Este boton aun no esta programado");
         }
 
         private void atencionToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Este boton aun no esta programado");
         }
 
         private void consultarToolStripMenuItem5_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Este boton aun no esta programado");
         }
 
         private void cargarProductoToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Este boton aun no esta programado");
         }
 
         private void listaDePreciosToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Este boton aun no esta programado");
         }
 
         private void stockToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Este boton aun no esta programado");
         }
 
         private void cargarToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Este boton aun no esta programado");
         }
 
         private void comprobanteToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Este boton aun no esta programado");
         }
 
         private void consultarToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Este boton aun no esta programado");
         }
 
         private void cargarClienteToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            //cargar cliente
         }
 
         private void editarToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            //para el update de clientes
         }
 
         private void consultarToolStripMenuItem3_Click(object sender, EventArgs e)
         {
-
+            //para consulta sobre clientes.
         }
 
         private void cargarMascotaToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            //cargar mascota
         }
 
         private void editarToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-
+            //para el update de mascotas
         }
 
         private void consultarToolStripMenuItem4_Click(object sender, EventArgs e)
         {
-
+            //para consultar sobre las mascotas 
         }
 
         private void salirToolStripMenuItem_Click(object sender, EventArgs e)
@@ -140,7 +141,7 @@ namespace Prueba3form
 
         private void MenuPrincipalForm_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
