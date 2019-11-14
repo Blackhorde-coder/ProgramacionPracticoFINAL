711d7c578a3ccaeeb4820f9a35fea76d38642536
Blackhorde <esingcomputacion@gmail.com>
2019-11-13 21:02:33 -0300

    Revert "asd"
    This reverts commit e64010a26aec014177ce33a001bb57af33cd4a7d.

--- Prueba3form/mascotacls.cs
+++ Prueba3form/mascotacls.cs
@@ -6,67 +6,8 @@ using System.Threading.Tasks;
 
 namespace Prueba3form
 {
-    class Mascotacls
+    class mascotacls
     {
-        private Clientecls _cliente;
-        private string _nombre;
-        private DateTime _fecNac;
-        private int _tipo;
-        private bool _sexo;
-        private string _descripcion;
-        //constructores 
-        public Mascotacls()
-        {
-            _nombre = "";
-            _fecNac = DateTime.Today;
-            _tipo = 0;
-            _sexo = true;
-            _descripcion = "";
-            _cliente = null;
-        }
-        public Mascotacls(string nombre, DateTime fecNac, int tipo, bool sexo, string descripcion, Clientecls c)
-        {
-            _nombre = nombre;
-            _fecNac = fecNac;
-            _tipo = tipo;
-            _sexo = sexo;
-            _descripcion = descripcion;
-            _cliente = c;
-        }
 
-        public string Nombre1 { get => _nombre; set => _nombre = value; }
-        public DateTime FecNac { get => _fecNac; set => _fecNac = value; }
-        public int Tipo { get => _tipo; set => _tipo = value; }
-        public string Descripcion { get => _descripcion; set => _descripcion = value; }
-        public Clientecls Cliente { get => _cliente; set => _cliente = value; }
-
-        //to string mascotas
-        public string toStringMascota()
-        {
-            return "Nombre: " + _nombre +
-                   "\nFecha de Nacimiento: " + _fecNac +
-                   "\nTipo: " + tipoanimal() +
-                   "\nDescripcion: " + _descripcion +
-                   "\nDatos del Cliente(Dueño): " + _cliente.toStringCliente();
-        }
-
-        public string tipoanimal()
-        {
-            string a = "";
-            switch(_tipo)
-            {
-                case 1: a = "Perro";
-                    break;
-                case 2: a = "Gato";
-                    break;
-                case 3: a = "Lombriz";
-                        break;
-                case 4: a = "Pez";
-                    break;
-                case 5: a = "Ave";
-                    break;
-            }
-            return a;
-        }
     }
 }
