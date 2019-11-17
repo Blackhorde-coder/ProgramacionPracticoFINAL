1a3d3f3e7b6bd245a573cc96345e23cbd5a90001
BlackhordeES <esingcomputacion@gmail.com>
2019-11-16 22:08:21 -0300

    123

--- Prueba3form/CargarMascotaForm.cs
+++ Prueba3form/CargarMascotaForm.cs
@@ -46,25 +46,38 @@ namespace Prueba3form
         private void btnCargar_Click(object sender, EventArgs e)
         {
             //hacer if para que siempre se tenga q seleccionar un cliente
-            Mascotacls m = new Mascotacls();
-            m.Nombre1 = txtNombre.Text;
-            m.FecNac = dtpFechaNac.Value;
-            m.Tipo = cmbTipo.SelectedIndex;
-            m.Descripcion = txtDescripcion.Text;
-            m.Cliente = arrayclientes[listCliente.SelectedIndex];
-            //cargar en la basde de datos
-            string consultasql = "insert into mascota (Nombre, fecha_nac, tipo, descripcion, cliente_id) values " +
-                                  "('" + m.Nombre1 + 
-                                  "','" + m.FecNac + 
-                                  "'," + m.Tipo +
-                                  ",'" + m.Descripcion +
-                                  "'," + m.Cliente.ClienteID + ")";
-            bd.modificarbd(consultasql);
-
-
-
+            if (validacion() == true)
+            {
+                Mascotacls m = new Mascotacls();
+                m.Nombre1 = txtNombre.Text;
+                m.FecNac = dtpFechaNac.Value;
+                m.Tipo = cmbTipo.SelectedIndex;
+                m.Descripcion = txtDescripcion.Text;
+                m.Cliente = arrayclientes[listCliente.SelectedIndex];
+                //cargar en la basde de datos
+                string consultasql = "insert into mascota (Nombre, fecha_nac, tipo, descripcion, cliente_id) values " +
+                                      "('" + m.Nombre1 +
+                                      "','" + m.FecNac +
+                                      "'," + m.Tipo +
+                                      ",'" + m.Descripcion +
+                                      "'," + m.Cliente.ClienteID + ")";
+                bd.modificarbd(consultasql);
+            }
         }
-
+        private bool validacion()
+        {
+            if (listCliente.SelectedIndex == -1)
+            {
+                MessageBox.Show("SELECCIONE UN CLIENTE", "CUIDAD");
+                return false;
+            }
+            if (cmbTipo.SelectedIndex == -1)
+            {
+                MessageBox.Show("SELECCIONE TIPO", "CUIDADO");
+                return false;
+            }
+            return true;
+        }
         private void btnCliente_Click(object sender, EventArgs e)
         {
             ClienteForm cc = new ClienteForm();
@@ -85,17 +98,28 @@ namespace Prueba3form
             while (bd.Reader.Read())
             {
                 Clientecls cli = new Clientecls();
-                cli.ClienteID = bd.Reader.GetInt32(0);
-                cli.Nombre = bd.Reader.GetString(1);
-                cli.Apellido = bd.Reader.GetString(2);
-                cli.Documento = bd.Reader.GetInt32(3);
-                cli.Sexo = bd.Reader.GetBoolean(4);
-                cli.Direccion1 = bd.Reader.GetString(5);
-                cli.Barrio1 = bd.Reader.GetString(6);
-                cli.Codfijo1 = bd.Reader.GetInt32(7);
-                cli.Fijo1 = bd.Reader.GetInt32(8);
-                cli.Codmovil = bd.Reader.GetInt32(9);
-                cli.Movil = bd.Reader.GetInt32(10);
+                if(!bd.Reader.IsDBNull(0))
+                    cli.ClienteID = bd.Reader.GetInt32(0);
+                if (!bd.Reader.IsDBNull(1))
+                    cli.Nombre = bd.Reader.GetString(1);
+                if (!bd.Reader.IsDBNull(2))
+                    cli.Apellido = bd.Reader.GetString(2);
+                if (!bd.Reader.IsDBNull(3))
+                    cli.Documento = bd.Reader.GetInt32(3);
+                if (!bd.Reader.IsDBNull(4))
+                    cli.Sexo = bd.Reader.GetBoolean(4);
+                if (!bd.Reader.IsDBNull(5))
+                    cli.Direccion1 = bd.Reader.GetString(5);
+                if (!bd.Reader.IsDBNull(6))
+                    cli.Barrio1 = bd.Reader.GetString(6);
+                if (!bd.Reader.IsDBNull(7))
+                    cli.Codfijo1 = bd.Reader.GetInt32(7);
+                if (!bd.Reader.IsDBNull(8))
+                    cli.Fijo1 = bd.Reader.GetInt32(8);
+                if (!bd.Reader.IsDBNull(9))
+                    cli.Codmovil = bd.Reader.GetInt32(9);
+                if (!bd.Reader.IsDBNull(10))
+                    cli.Movil = bd.Reader.GetInt32(10);
                 arrayclientes[c] = cli;
                 c++;
             }
